6a5f3be3e6f6c55726eeb3adffb9538b6237ea3e
danisnowman <danielernesto@ufm.edu>
2021-04-13 20:18:19 -0600

    Improves printing

--- mat_mult.py
+++ mat_mult.py
@@ -6,8 +6,8 @@ class mat_mult:
 
     @staticmethod
     def mult(file_a, file_b):
-        print(tabulate(file_a, headers='keys', tablefmt='psql'))
-        print(tabulate(file_b, headers='keys', tablefmt='psql'))
+        print(tabulate(file_a, headers='keys', tablefmt='pretty'))
+        print(tabulate(file_b, headers='keys', tablefmt='pretty'))
 
         print(f"FILE A {file_a}")
         print(f"FILE B {file_b}")
