1b3f0c128b960ccb85ad4857a5b8d8eddabc12f7
danisnowman <danielernesto@ufm.edu>
2021-04-13 20:27:27 -0600

    adds initial logic

--- mat_mult.py
+++ mat_mult.py
@@ -1,13 +1,42 @@
 import pandas as pd
 from tabulate import tabulate
+import threading
+import time
 
 
 class mat_mult:
+    def __init__(self, matrix_a, matrix_b):
+        """ Create a new point at the origin """
+        self.X = matrix_a
+        self.Y = matrix_b
 
-    @staticmethod
-    def mult(file_a, file_b):
-        print(tabulate(file_a, headers='keys', tablefmt='pretty'))
-        print(tabulate(file_b, headers='keys', tablefmt='pretty'))
 
-        print(f"FILE A {file_a}")
-        print(f"FILE B {file_b}")
+    def mult(self, file_a, file_b):
+        result = [[0]*self.m]
+        for z in range(len(self.Y[0])):
+            for k in range(len(self.Y)):
+                result[0][z] += self.X[0] * self.Y[k][z]
+
+        threads = list()
+
+
+        start = time.perf_counter()
+        for i in range(len(self.X[0])):
+            self.X = threading.Thread(target=self.mult, args=(self.X[i], self.Y))
+            threads.append(self.X)
+            self.X.start()
+
+        for index, thread in enumerate(threads):
+            thread.join()
+        end = time.perf_counter()
+
+        print(
+            f"Time taken to complete mult() {self.m}x{self.m}: {round(end - start, 5)} seconds(s)")
+   
+   
+   # print(f" {result}")
+        # print(tabulate(file_a, headers='keys', tablefmt='pretty'))
+        # print(tabulate(file_b, headers='keys', tablefmt='pretty'))
+
+        # print(f"FILE A {file_a}")
+        # print(f"FILE B {file_b}")
